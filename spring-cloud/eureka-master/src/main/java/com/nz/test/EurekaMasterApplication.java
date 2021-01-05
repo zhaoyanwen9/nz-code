c60d3377ced6f12cd9de25f116bd4a5ffdf10260
zhaoyanwen9 <1073159598@qq.com>
2021-01-05 21:06:27 +0800

    清理无用代码

--- spring-cloud/eureka-master/src/main/java/com/nz/test/EurekaMasterApplication.java
+++ spring-cloud/eureka-master/src/main/java/com/nz/test/EurekaMasterApplication.java
@@ -4,12 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * @author nz
- * @EnableEurekaServer - 激活eureka服务器相关配置
- * EurekaServerMarkerConfiguration
- */
-
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaMasterApplication {
--- spring-cloud/task-mybatis/src/main/java/com/nz/test/service/AccountService.java
+++
@@ -1,7 +0,0 @@
-package com.nz.test.service;
-
-public interface AccountService {
-
-    void transfer(int outter, int inner, Integer money);
-
-}
